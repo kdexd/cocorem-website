0e3d9b04c624ee47688afb2278b181808ba9fe5a
Shweta <shwetasinghshweta540@gmail.com>
2024-01-15 15:37:13 +0530

    image id suggestions

--- explore-app/src/components/ImageInput.js
+++ explore-app/src/components/ImageInput.js
@@ -1,43 +1,91 @@
 import React, { useState } from 'react';
 
-const ImageInput = ({ onImageIdChange}) => {
+const ImageInput = ({ onImageIdChange }) => {
   const [imageId, setImageId] = useState('');
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
+  const suggestions = ['139', '285', '1000', '2149','13004','2592','26926','45472','50844','54628','79408','82807'];
 
-  const handleChange = (event) => {
-    const enteredValue = event.target.value;
-    
-    // Remove leading zeros and update the state
-    setImageId(enteredValue.replace(/^0+/, ''));
-    // const paddedValue = enteredValue.padStart(12, '0');
-    // onImageIdChange(paddedValue);
-  
+  const handleChange = (event) => setImageId(event.target.value.replace(/^0+/, ''));
+
+  const handleSuggestionClick = (suggestion) => {
+    setImageId(suggestion);
+    onImageIdChange(suggestion.padStart(12, '0'));
   };
+
+  const handleSearchFocus = () => setIsSearchFocused(true);
+  const handleSearchBlur = () => setIsSearchFocused(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const paddedValue = imageId.padStart(12, '0');
-    onImageIdChange(paddedValue);
+    onImageIdChange(imageId.padStart(12, '0'));
   };
 
+  const getRandomSuggestions = () => suggestions.sort(() => Math.random() - 0.5).slice(0, 5);
+
   return (
-    <div>
-      <div class="container">
+    <div className="container my-3 d-flex justify-content-center">
       <form onSubmit={handleSubmit} className="d-flex">
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Enter Image ID"
-          aria-label="Search"
-          value={imageId}
-          onChange={handleChange}
-        />
-        <button className="btn btn-outline-success" type="submit">
+        <div style={{ position: 'relative', width: '400px', display: 'flex' }}>
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Enter Image ID"
+            aria-label="Search"
+            value={imageId}
+            onChange={handleChange}
+            onFocus={handleSearchFocus}
+            onBlur={handleSearchBlur}
+            list="suggestions"
+            style={{
+              width: '100%',
+              borderRadius: '5px',
+              border: '1px solid #28a745',
+              marginRight: '5px',
+              padding: '5px',
+              color: '#029E73',
+            }}
+          />
+          {isSearchFocused && suggestions.length > 0 && (
+            <div
+              style={{
+                position: 'absolute',
+                top: '100%',
+                left: 0,
+                zIndex: 2,
+                width: '100%',
+                backgroundColor: 'white',
+                borderRadius: '5px',
+                border: '1px solid',
+                marginTop: '5px',
+              }}
+            >
+              {getRandomSuggestions().map((suggest, index) => (
+                <div
+                  key={index}
+                  onMouseDown={() => handleSuggestionClick(suggest)}
+                  style={{
+                    cursor: 'pointer',
+                    padding: '8px 10px',
+                    borderBottom: '1px solid #28a745',
+                    color: '#029E73',
+                  }}
+                >
+                  {suggest}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+        <button
+          className="btn btn-outline-success"
+          type="submit"
+          style={{ backgroundColor: '#029E73', color: '#ffffff', borderRadius: '5px' }}
+        >
           Search
         </button>
       </form>
     </div>
-    </div>
   );
 };
-
 
 export default ImageInput;
